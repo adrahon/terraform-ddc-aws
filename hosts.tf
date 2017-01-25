b1d302f2ff7ea3d504fcc84e6a6d4c1a98c8f8f2
Alex Drahon <adrahon@gmail.com>
2017-01-25 11:00:56 +0100

    Split hosts and security groups

---
+++ hosts.tf
@@ -0,0 +1,46 @@
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "${var.aws_region}"
+}
+
+resource "aws_instance" "ucp-manager" {
+  ami           = "${lookup(var.aws_amis, var.aws_region)}"
+  instance_type = "${var.manager_instance_type}"
+  key_name      = "${var.key_name}"
+
+  root_block_device {
+    volume_type = "gp2"
+    delete_on_termination = true
+  }
+
+  count = "${var.ucp_manager_count}"
+
+  security_groups = [ "${aws_security_group.ddc.name}" ]
+  availability_zone = "${element(split(",", var.availability_zones), count.index) }"
+
+  tags {
+    Name = "${var.name_prefix}_ucp-manager${count.index + 1}"
+  }
+}
+
+resource "aws_instance" "ucp-worker" {
+  ami           = "${lookup(var.aws_amis, var.aws_region)}"
+  instance_type = "${var.worker_instance_type}"
+  key_name      = "${var.key_name}"
+
+  root_block_device {
+    volume_type = "gp2"
+    delete_on_termination = true
+  }
+
+  count = "${var.ucp_worker_count}"
+
+  security_groups = [ "${aws_security_group.ddc.name}" ]
+  availability_zone = "${element(split(",", var.availability_zones), count.index) }"
+
+  tags {
+    Name = "${var.name_prefix}_ucp-node${count.index + 1}"
+  }
+}
+
